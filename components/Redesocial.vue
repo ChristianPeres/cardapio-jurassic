ecc6082e38ad9f1016e92788804904100dd45876
CHRISTIAN <christianperesg@gmail.com>
2024-10-17 10:44:00 -0400

    adicionando rede social

--- components/Redesocial.vue
+++ components/Redesocial.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="w-full md:w-5/5 mx-auto py-12 relative">
       <h1 class="text-4xl font-bold text-center mt-4 mb-0 text-bg-custom-green">Nos siga na nossa rede social</h1> 
-      <div class="flex flex-wrap justify-center gap-3 mt-16 text-4xl">
-      <div v-for="(midias, idx) in midia" :key="idx" class="flex items-center px-4 py-1 mb-16"> 
-              <a class="mt-10 text-white flex justify-center py-1 gap-2 items-center" :href="midias.link">
+      <div class="flex flex-wrap justify-center gap-3 mt-5 text-4xl">
+      <div v-for="(midias, idx) in midia" :key="idx" class="flex items-center px-4 py-1 mb-10"> 
+              <a class="mt-1 text-white flex justify-center py-1 gap-2 items-center" :href="midias.link">
                   <span v-html="midias.icone(40, '#ffffff')"></span>
                   {{ midias.name }}
               </a>
